d4a128cee2c7ef4653470dc52a1e4cd91d961ad6
Joshua Andes <andesjoshua@comcast.net>
2021-09-22 10:32:47 -0400

    commit tree 81d08b9297577305259bb8bbbf3877de26da2f9e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+const findMatching = (driverName, argument) => {
+    return driverName.filter(name => {
+        if(name.includes(argument)) {
+            return name
+        } else if(name.includes(argument.toLowerCase())) {
+            return name
+        }
+    })
+}
+const fuzzyMatch = (drivers, argument) => {
+    return drivers.filter(name => {
+        if(name[0] === argument[0] && name[1] === argument[1]) {
+            return name
+        }
+    })
+}
+const matchName = (drivers, argument) => {
+    drivers.filter((driverName) => {
+        if(driverName.name === argument) return driverName
+    })
+    
+}
